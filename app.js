0c5e411784d5e5c67daae691df6ea3a5c2d773a3
Rikin Katyal <katyalrikin@gmail.com>
2015-09-28 21:31:07 -0400

    testing

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(bodyParser.urlencoded({
 app.post('/link', function (req, res) {
     var linkKey = req.body.linkKey;
     var link = req.body.linkURL;
-    con.query("INSERT INTO links (demo, yo) VALUES (linkkey, finallink)", function(err) {
+    con.query("INSERT INTO links (linkkey, finallink) VALUES (linkkey, finallink)", function(err) {
         console.log(err);
     })
 });
