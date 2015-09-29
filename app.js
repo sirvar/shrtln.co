a0e3497db8a22a0a881701a0ea35750bab2faf9f
Rikin Katyal <katyalrikin@gmail.com>
2015-09-28 20:21:25 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,41 @@
+// Import modules
+var express = require('express');
+var app = express();
+var path = require('path');
+var http = require('http').Server(app);
+var mysql = require("mysql");
+
+var con = mysql.createConnection({
+	host: "http://shrtln.co",
+	database: "shrtlnco",
+	user: "root",
+	password: "shrtlnco"
+});
+
+con.connect(function(err){
+  if(err){
+    console.log('Error connecting to Db');
+    return;
+  }
+  console.log('Connection established');
+});
+
+con.end(function(err) {
+  // The connection is terminated gracefully
+  // Ensures all previously enqueued queries are still
+  // before sending a COM_QUIT packet to the MySQL server.
+});
+
+// Get all files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+
+// Listen on port 7002
+http.listen(7002, function() {
+    console.log('listening on port 7002');
+});
