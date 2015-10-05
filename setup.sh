8cd054a09b3e1e31458c33ba725357ba6876e58f
Paul K <happy2serve@yahoo.ca>
2015-10-04 22:42:14 -0400

    update

--- setup.sh
+++ setup.sh
@@ -1,9 +1,2 @@
 #!/bin/bash
-mysql -u root [username] -p[password] << EOF
-CREATE DATABASE shrtlnco;
-USE shrtlnco;
-CREATE TABLE links (
-	linkkey VARCHAR(65536) NOT NULL,
-	finallink VARCHAR(65535) NOT NULL
-)
-EOF
+mysql -u root -p < commands.txt
